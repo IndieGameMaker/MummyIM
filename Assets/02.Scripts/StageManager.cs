98215e16cbcdadfb83cd80abf994ebd18392aab7
Lee JaeHyun <myleje@gmail.com>
2020-08-18 11:17:15 +0900

    .

--- Assets/02.Scripts/StageManager.cs
+++ Assets/02.Scripts/StageManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    public enum HINT_COLOR
+    {
+        RED = 0,
+        GREEN = 1,
+        BLUE = 2
+    }
+    public HINT_COLOR hintColor;
+
+    public GameObject[] hintPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
